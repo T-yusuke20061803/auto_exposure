a7d2ecc328679d60826fa1a9ebf1fd6905beca24
T-yusuke20061803 <t.yusuke0618@icloud.com>
2025-10-07 18:02:48 +0900

    10/07モデル縮小EfficientNet

--- src/model.py
+++ src/model.py
@@ -187,17 +187,17 @@ class ResNet(nn.Module):
         else:
             raise NotImplementedError()
 
-        self.in_planes = 16
+        self.in_planes = 32
         self.down_sampling_layer = down_sampling_layer
 
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1,
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1,
                                padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 128, num_blocks[3], stride=2)
-        self.linear = nn.Linear(128*block.expansion, 1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.layer1 = self._make_layer(block, 32, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 64, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 128, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 256, num_blocks[3], stride=2)
+        self.linear = nn.Linear(256*block.expansion, 1)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
