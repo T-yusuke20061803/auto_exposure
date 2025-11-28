03dcccea68c09ca4e700f42ac6961877c3c6687f
T-yusuke20061803 <t.yusuke0618@icloud.com>
2025-11-28 19:39:16 +0900

    11/28評価用変化量画像

--- scripts/check_match.py
+++ scripts/check_match.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import sys
 
 # ================= 設定 =================
-csv_path = Path("conf/dataset/HDR+burst_split/train.csv")
+csv_path = Path("conf/dataset/HDR+burst_split/val.csv")
 image_root = Path("conf/dataset/HDR+burst/processed_1024px_exr")
 # ========================================
 
